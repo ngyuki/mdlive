5cfa9f4e808fcf4a9a877db38dfb76d97bd9d615
ngyuki <ngyuki.jp@gmail.com>
2019-02-24 23:24:10 +0900

    Use class in markdown.js for marked.Renderer

--- src/markdown.js
+++ src/markdown.js
@@ -2,26 +2,25 @@ import fs from 'fs';
 import marked from 'marked';
 import hljs from 'highlight.js';
 
-function renderer() {
-    return Object.assign(new marked.Renderer(), {
-        image(href, title, text) {
-            const filename = decodeURIComponent(href);
-            if (!/^[a-z]+:\/\//.test(filename)) {
-                try {
-                    const data = fs.readFileSync(filename).toString('base64');
-                    return `<img alt="${text}" src="data:image/png;base64,${data}">`;
-                } catch (err) {
-                    // noop
-                }
+class Renderer extends marked.Renderer {
+
+    image(href, title, text) {
+        const filename = decodeURIComponent(href);
+        if (!/^[a-z]+:\/\//.test(filename)) {
+            try {
+                const data = fs.readFileSync(filename).toString('base64');
+                return `<img alt="${text}" src="data:image/png;base64,${data}">`;
+            } catch (err) {
+                // noop
             }
-            return marked.Renderer.prototype.image.call(this, href, title, text);
         }
-    });
+        return super.image(href, title, text);
+    }
 }
 
 function parseString(input, embedded) {
     return marked(input, {
-        renderer: embedded ? renderer() : null,
+        renderer: embedded ? new Renderer() : null,
         highlight: (code, lang) => {
             return hljs.highlightAuto(code, [lang]).value;
         },
